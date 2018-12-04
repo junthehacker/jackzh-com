856597e7bdb2fc344d0b0185b1ead46714790570
Jun Zheng <me@jackzh.com>
2018-12-04 06:16:00 -0500

    publish: ✨ Add sitemap.xml generation feature
    generated from commit ad2bcdf6968e134c746117674dc6b81bf3673814

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
 (function(){
-
+    renderMathInElement(
+        document.body,
+        {
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "\\[", right: "\\]", display: true},
+                {left: "$", right: "$", display: false},
+                {left: "\\(", right: "\\)", display: false}
+            ]
+        }
+    );
 })();
