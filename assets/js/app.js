c7edb4957c883daefc516f205ee9bfb4692a01c8
Jun Zheng <me@jackzh.com>
2020-01-16 16:54:29 -0500

    publish: Add better mobile navigation (#3)
    generated from commit 1b7080a3aa7c35808fec79b9fee6b5709f3599c1

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,4 +10,15 @@
             ]
         }
     );
+
+    $("#mobile-nav-button").click(function() {
+        const btnRef = $("#mobile-nav-button");
+        btnRef.toggleClass("expanded");
+        if(btnRef.hasClass("expanded")) {
+            btnRef.html('<i class="fa fa-angle-up" aria-hidden="true"></i> Navigation')
+        } else {
+            btnRef.html('<i class="fa fa-angle-down" aria-hidden="true"></i> Navigation')
+        }
+        $(".navbar").toggleClass("expanded");
+    })
 })();
